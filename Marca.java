7f0518c49b788966c701ebc8eb6fd717a217b7f4
AlbertoDuarte <180011707@aluno.unb.br>
2019-04-26 08:52:25 -0300

    Implement Marca class basic methods and attributes

--- Marca.java
+++ Marca.java
@@ -5,12 +5,46 @@
  */
 package trabalhow;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author root
  */
 public class Marca {
     int id;
-    String nome;
-    Modelo[] modelos;
+    private String nome;
+    private ArrayList<Modelo> modelos;
+
+    Marca(String nome) {
+        this.nome = nome.toLowerCase();
+        this.modelos = new ArrayList<>();
+
+    }
+    
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void adicionarModelo(Modelo modelo) {
+        for (Modelo i : this.modelos) {
+            if(i.nome.equals(modelo.nome)) {
+                System.out.println("Modelo ja esta na marca!");
+                return;
+            }
+        }
+        this.modelos.add(modelo);
+    }
+    
+    public void removerModelo(Modelo modelo) {
+        boolean s;
+        s = this.modelos.remove(modelo);
+        if(s) {
+            System.out.println("Modelo removido com sucesso");
+        }
+        else {
+            System.out.println("Modelo nao encontrado");
+        }
+    }
 }
